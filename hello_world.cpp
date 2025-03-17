46028ce554532d6ab69bc16848ed9d302eb49227
Александр Лучук <aleksandrluchuk190@gmail.com>
2025-03-17 21:11:39 +0300

    В код добавлены комментарии

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,16 @@
 
 int main()
 {
+    // Объявляем переменную для хранения имени пользователя
     std::string name;
+
+    // Запрашиваем имя пользователя
     std::cout << "Enter your name, please: ";
     std::cin >> name;
+
+    // Выводим приветствие
     std::cout << "Hello world from " << name << "!" << std::endl;
+
+    // Завершаем программу успешно
     return 0;
 }
